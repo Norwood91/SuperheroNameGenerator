87e6196d724cc728bd885e9e54b5e0bf8390882f
Robert_Norwood <robnorwood91@gmail.com>
2021-05-27 03:10:00 -0700

    The project is done

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello world!")
+print("Hello, and welcome to the Superhero Name Generator!")
+
+prefix = "The"
+
+adjective = input("What is one word that best describes you?\n")
+
+animal = input("What is the name of your favorite animal?\n")
+
+print("Your superhero name is: " + prefix + " " + adjective + " " + animal + "!")
+
+
